cc658930226312d46a5bb121bb6cc7feac29f1a8
allexib <ooomegasib@gmail.com>
2022-04-13 17:07:24 +0300

    css,searchIcon,MovieLAnd

--- findmovie/src/App.js
+++ findmovie/src/App.js
@@ -1,5 +1,8 @@
 import {useEffect} from 'react'
 
+import './App.css'
+import SearchIcon from './search.svg'
+
 const API_URL = 'http://www.omdbapi.com?apikey=1f866f3f'
 
 const App = () => {
@@ -8,14 +11,16 @@ const App = () => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json()
 
-        console.log(data)
+        console.log(data.Search)
     }
 
     useEffect(() => {
         searchMovies('Spiderman')
     }, [])
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+        </div>
     )
 }
 
