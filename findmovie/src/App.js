18ee2767aae7b993fe63d5b5b44dc7b231444145
allexib <ooomegasib@gmail.com>
2022-04-13 17:41:57 +0300

    finish, state, hooks, props, simple react

--- findmovie/src/App.js
+++ findmovie/src/App.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useState, useEffect} from 'react'
 
 import MovieCard from './MovieCard'
 import './App.css'
@@ -15,12 +15,14 @@ const movie1 = {
 }
 
 const App = () => {
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json()
 
-        console.log(data.Search)
+        setMovies(data.Search)
     }
 
     useEffect(() => {
@@ -33,21 +35,30 @@ const App = () => {
             <div className='search'>
                 <input
                     placeholder='Search for movies'
-                    value='kama'
-                    onChange={() => {
-                    }}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt='search'
-                    onClick={() => {
-                    }}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            <div className='container'>
-                <MovieCard movie1={movie1}/>
-            </div>
+            {movies?.length > 0
+                ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )}
+
+
         </div>
     )
 }
